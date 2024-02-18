aa3c69270d572b19acc6c651aa7b12b8de5cdf41
Denisoed <denisoed@gmal.com>
2024-02-18 16:01:42 +0600

    Added Experience section

---
+++ app/components/Resume.vue
@@ -0,0 +1,61 @@
+<template>
+  <section class="resume2 section-padding wow fadeIn">
+    <div class="container">
+      <div class="sec-head mb-80 wow fadeIn">
+        <div class="row justify-content-center">
+          <div class="col-lg-8 text-center">
+            <div class="d-inline-block">
+              <div class="sub-title-icon d-flex align-items-center">
+                <span class="icon pe-7s-chat"></span>
+                <h6>My Resume</h6>
+              </div>
+            </div>
+            <h3>Education & Experience</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 valign">
+          <div>
+            <div class="resume-clumn">
+              <div class="items mb-40">
+                <h6 class="date fz-14">2018 - Present</h6>
+                <h5>Art Director</h5>
+                <span class="opacity-8 fw-500 mt-10">[ at Ui-Themez ]</span>
+                <p class="fz-13 mt-15">Crafting captivating digital
+                  experiences that put users
+                  at the heart of the design. Elevate your product to
+                  increased user
+                  satisfaction and loyalty.</p>
+              </div>
+              <div class="items mb-40">
+                <h6 class="date fz-14">2015 - 2017</h6>
+                <h5>Front-end Developer</h5>
+                <span class="opacity-8 fw-500 mt-10">[ at Envato Market
+                  ]</span>
+                <p class="fz-13 mt-15">Crafting captivating digital
+                  experiences that put users
+                  at the heart of the design. Elevate your product to
+                  increased user
+                  satisfaction and loyalty.</p>
+              </div>
+              <div class="items">
+                <h6 class="date fz-14">2010 - 2014</h6>
+                <h5>Bachelor's Degree in Design</h5>
+                <span class="opacity-8 fw-500 mt-10">[ US RMIT University
+                  ]</span>
+                <p class="fz-13 mt-15">Crafting captivating digital
+                  experiences that put users
+                  at the heart of the design. Elevate your product to
+                  increased user
+                  satisfaction and loyalty.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
